74ea8d0d045a30ca3dfe8418a236c31efe9f76de
cyvexx <shanelucaslin@gmail.com>
2026-02-17 16:12:51 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+// Counter
+let count = 0;
+
+function increase() {
+  count = count + 1;
+  document.getElementById("count").innerText = count;
+}
+
+function resetCount() {
+  count = 0;
+  document.getElementById("count").innerText = count;
+}
+
+// Task List
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value;
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerText = taskText;
+
+  li.onclick = function() {
+    li.style.textDecoration = "line-through";
+  };
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
+
